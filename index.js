b538bc172566a9e64938979be946cc79c00af6fc
greg-Da <gregoiredalmasso@gmail.com>
2020-11-09 14:24:00 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+let firstName = "Gregoire";
+
+let favouriteColor = "blue";
+
+let favouriteFood = "Pizza";
+let city = "Paris";
+
+//Display my first name, my favourite color and my favorite food
+console.log(firstName);
+console.log(favouriteColor);
+console.log(favouriteFood);
+
+
+let phrase = "A rainbow is formed by the sunlight. Shining through rain drops so small. The rain bends the light from the sunshine, and colors the sky for us all."
+let endingPhrase = "is what we see. Rainbow colours, are made with light bending you see!"
+let one = "Red"
+let two = "orange"
+let three = "yellow"
+let four = "green"
+let five = "blue"
+let six = "indigo"
+let seven = "violet"
+
+console.log(phrase + one + ", " + two + ", " + three + ", " + four + ", " + five + ", " + six + ", " + seven + ", " + endingPhrase)
+
+function myAlphabetLength() {
+    var myAlphabet = ['A', 'B', 'C', 'D','E','F', 'G'];
+    console.log(myAlphabet.length);
+    
+}
+
+myAlphabetLength();
